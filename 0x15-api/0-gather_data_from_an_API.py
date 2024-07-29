abc1e86ff677e65031812ee7e0ffa4f9976aa5cb
Praise Aribisala <aribisalapraise12@gmail.com>
2024-07-29 23:10:25 +0100

    Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,32 +1,46 @@
 #!/usr/bin/python3
 """"
-A API request project
+A Script to fetch and display completed tasks for a user
+from a REST API
 """
 
 import requests
 import sys
 
 
-id = sys.argv[1]
-response1 = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(id)
-)
-response2 = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id)
-)
-result1 = response1.json()
-result2 = response2.json()
+def main():
+    """Fetch an display complted tasks for a user."""
+    if len(sys.argv) != 2:
+        print("Usage: python3 <name_of_file.py> <employee_id>")
+        return
 
-EMPLOYEE_NAME = result1['name']
-TOTAL_NUMBER_OF_TASKS = len(result2)
-task_titles = [item["title"] for item in result2 if item["completed"]]
-NUMBER_OF_DONE_TASKS = len(task_titles)
+    id = sys.argv[1]
+    response1 = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+    )
+    response2 = requests.get(
+            'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id)
+    )
+
+    if response1.status_code != 200 or response2.status_code != 200:
+        print("Error fetching data from the API")
+        return
+
+    user = response1.json()
+    tasks = response2.json()
+
+    EMPLOYEE_NAME = user.get('name')
+    TOTAL_NUMBER_OF_TASKS = len(tasks)
+    task_titles = [item["title"] for item in tasks if item.get("completed")]
+    NUMBER_OF_DONE_TASKS = len(task_titles)
+
+    print(
+            "Employee {} is done with tasks({}/{}):"
+            .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS)
+    )
+    for title in task_titles:
+        print("\t {}".format(title))
 
 
-print(
-        "Employee {} is done with tasks({}/{}):"
-        .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS)
-)
-
-for title in task_titles:
-    print("\t {}".format(title))
+if __name__ == "__main__":
+    main()

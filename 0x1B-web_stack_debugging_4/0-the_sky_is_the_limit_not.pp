00a826d7a00c319bb9adde0100edaba6f86d5cf6
Praise Aribisala <aribisalapraise12@gmail.com>
2024-08-23 22:05:53 +0100

    Task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,28 +1,6 @@
 # fix nginx to accept and serve more requests
 
-#exec {'modify max open files limit setting':
-#  command => 'sed -i "s/15/4096" /etc/default/nginx && sudo nginx restart',
-#  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
-#}
-
-exec { 'modify max open files limit setting':
-  command => 'sed -i "s/#worker_rlimit_nofile.*/worker_rlimit_nofile 4096;/" /etc/nginx/nginx.conf',
+exec {'modify max open files limit setting':
+  command => 'sed -i "s/15/4096" /etc/default/nginx && sudo nginx restart',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
-
-exec { 'modify worker processes':
-  command => 'sed -i "s/worker_processes.*/worker_processes auto;/" /etc/nginx/nginx.conf',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
-}
-
-exec { 'modify worker connections':
-  command => 'sed -i "s/worker_connections.*/worker_connections 4096;/" /etc/nginx/nginx.conf',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
-}
-
-exec { 'restart nginx':
-  command => 'sudo systemctl restart nginx',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
-  require => [Exec['modify max open files limit setting'], Exec['modify worker processes'], Exec['modify worker connections']],
-}
-
